a286ce57b2d8327d317e9df9a4ae2a12953afa67
arnau lucq <arnau.lucq@hotmail.com>
2018-10-06 17:15:42 +0200

    ajout de l'attenuation de pluie

--- src/RainAttenuation.py
+++ src/RainAttenuation.py
@@ -1,12 +1,9 @@
 #! /usr/bin/python3
 
-if __name__=="__main__":
-	from SimulationObjects import *
-else:
-	from src.SimulationObjects import *
-	from src.SimulationComputation import *
 
-	from math import *
+
+from math import *
+	
 
 #TODO make all comments (why class, units of each parameters & where come from the formula)
 
@@ -21,6 +18,10 @@ class RainSpecificAttenuation :
         alphav_coefficient= [[-0.07771,2.33840,-0.76284],[0.56727,0.95545,0.54039],[-0.20238,1.14520,0.26809],[-48.2991,0.791669,0.116226],[45.5833,0.791459,0.116479],-0.053739,0.83433]
         tau=45
         theta=30
+        hr=420  #altitude nanosatellite en km
+        hs=0.3 #altitude station sol en km
+        Re= 8500 #effective radius of the earth
+        phi=5.1 #°N
         
 
         def formuleCoefficient(self,matrice,f):
@@ -28,22 +29,55 @@ class RainSpecificAttenuation :
                 for i in range(len(matrice)-2):
                         somme+= matrice[i][0]*exp(-((log10(f)-matrice[i][1])/matrice[i][2])**2)
                     
-                        print(somme)
                 somme+= matrice[-2]*log10(f) + matrice[-1]
                 
                 return somme
+
+        def longueurEffectivePluie(self,frequence):
+                
+                if self.theta < 5:
+                        ls = (2*(self.hr-self.hs))/((sin(self.theta*pi/180)*sin(self.theta*pi/180) + (2*(self.hr-self.hs))/Re)**0.5 + sin(self.theta*pi/180))
+                else:
+                        ls = (self.hr-self.hs)/sin(self.theta*pi/180)
+
+                lg=ls*cos(self.theta*pi/180)
+
+                r0_01=1/(1+0.78*(lg*self.rain_specific_attenuation/frequence)**0.5 - 0.38*(1-exp(-2*lg)))
+                
+                z=degrees(atan((self.hr-self.hs)/(lg*r0_01)))
+
+                if z>self.theta:
+                        lr=lg*r0_01/cos(self.theta*pi/180)
+                else:
+                        lr=(self.hr-self.hs)/sin(self.theta*pi/180)
+
+                if abs(self.phi)<36:
+                        X=36-abs(self.phi)
+                else:
+                        X=0
+
+                v0_01 = 1/(1+sqrt(sin(self.theta*pi/180))*(31*(1-exp(-self.theta/(1+X)))*(sqrt(lr*self.rain_specific_attenuation)/frequence**2)-0.45))
+
+                le=lr*v0_01
+                return(v0_01,le)
+
+        
         
 
         
-        def __init__(self,r0_01_rainfall_rate,frequence,theta):
+        def __init__(self,r0_01_rainfall_rate,frequence):
 
                 self.kh = pow(10,self.formuleCoefficient(self.kh_coefficient,frequence))
                 self.kv = pow(10,self.formuleCoefficient(self.kv_coefficient,frequence))
                 self.alphah = self.formuleCoefficient(self.alphah_coefficient,frequence)
                 self.alphav = self.formuleCoefficient(self.alphav_coefficient,frequence)
                 self.k = (self.kh + self.kv + (self.kh - self.kv)*cos(self.theta*pi/180)*cos(self.theta*pi/180)*cos(2*self.tau*pi/180))/2
-                self.alpha = (self.kh*self.alphah + self.kv*self.alphav + (self.kh*self.alphah - self.kv*self.alphav)*cos(self.theta*pi/180)*cos(self.theta*pi/180)*cos(2*self.tau*pi/180))/2*self.k
+                self.alpha = (self.kh*self.alphah + self.kv*self.alphav + (self.kh*self.alphah - self.kv*self.alphav)*cos(self.theta*pi/180)*cos(self.theta*pi/180)*cos(2*self.tau*pi/180))/(2*self.k)
                 self.rain_specific_attenuation = self.k*pow(r0_01_rainfall_rate,self.alpha)
+                self.a0_01_attenuation = self.rain_specific_attenuation*self.longueurEffectivePluie(frequence)[1]
+                self.a1_attenuation = self.a0_01_attenuation*pow(100,-(0.655 + 0.033*log(1)-0.045*log(self.a0_01_attenuation)))
 
 
--5.33980*exp(-((log10(8.2)+0.10008)/1.13098)**2)
+
+        
+        
